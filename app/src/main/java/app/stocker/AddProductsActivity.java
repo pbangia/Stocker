f36a9df4a114038421a07b34bbbb562e3e6703cf
Priyankit Bangia <priyankit.bangia@gmail.com>
2017-03-01 23:19:05 +1300

    Base implementation of category selection in product view

--- app/src/main/java/app/stocker/AddProductsActivity.java
+++ app/src/main/java/app/stocker/AddProductsActivity.java
@@ -1,17 +1,25 @@
 package app.stocker;
 
+import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import app.stocker.data.Product;
 import com.google.gson.Gson;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Locale;
 
 public class AddProductsActivity extends AppCompatActivity {
@@ -34,6 +42,30 @@ public class AddProductsActivity extends AppCompatActivity {
             price.setText(String.format(Locale.getDefault(),"%.2f", product.getPrice()));
             notes.setText(product.getNotes());
         }
+
+        final Spinner category = (Spinner) findViewById(R.id.edit_product_category);
+        List<String> list = new ArrayList<String>();
+        list.add("1");
+        list.add("2");
+        list.add("3");
+        ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_spinner_item, list);
+        dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        category.setAdapter(dataAdapter);
+        category.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View viewClicked, int pos,
+                                    long id) {
+                Toast.makeText(getBaseContext(),category.getSelectedItem().toString(), Toast.LENGTH_SHORT).show();
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> arg0) {
+                // TODO Auto-generated method stub
+            }
+        });
 
 
     }
